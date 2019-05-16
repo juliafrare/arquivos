2fe7bd0c3a1a3ba3562bd2fc9ea0dd054b7c86b5
juliafrare <juliafrare7@gmail.com>
2019-05-16 16:53:01 -0300

    funcionalidade 2 modificada p/ casos com arquivos removidos

--- t2/funcionalidade2.c
+++ t2/funcionalidade2.c
@@ -41,75 +41,86 @@ void leArquivoBin(char *nomeArquivo){
 			return;
 		}
 		
-		if(pagina[0] == '-'){
+		if(pagina[0] == '-' || pagina[0] == '*'){
 			while(offset < 32000){					//o ultimo byte da pagina esta no offset 32000 - 1 (ou seja, pagina[31999])
 				Dados d;
 
 				//copia dos bytes da pagina de disco para a struct do registro de dados
-				memcpy(&d.removido, &pagina[offset], 1);
-				offset += 1;
-				memcpy(&d.tamanhoRegistro, &pagina[offset], 4);
-				offset += 4;
-				memcpy(&d.encadeamentoLista, &pagina[offset], 8);
-				offset += 8;
-				memcpy(&d.idServidor, &pagina[offset], 4);
-				offset += 4;
-				memcpy(&d.salarioServidor, &pagina[offset], 8);
-				offset += 8;
-				memcpy(&d.telefoneServidor, &pagina[offset], 14);
-				offset += 14;
-
-				if(pagina[offset + 4] == 'n'){		//verifica se o campo nomeServidor existe verificando a presenca da tag 'n'
-					/*caso o campo existir, os dados sao copiados normalmente*/
-					memcpy(&d.tamNomeServidor, &pagina[offset], 4);
-					offset += 4;
-					memcpy(&d.tagCampo4, &pagina[offset], 1);
+				if(pagina[offset] == '-'){
+					memcpy(&d.removido, &pagina[offset], 1);
 					offset += 1;
+					memcpy(&d.tamanhoRegistro, &pagina[offset], 4);
+					offset += 4;
+					memcpy(&d.encadeamentoLista, &pagina[offset], 8);
+					offset += 8;
+					memcpy(&d.idServidor, &pagina[offset], 4);
+					offset += 4;
+					memcpy(&d.salarioServidor, &pagina[offset], 8);
+					offset += 8;
+					memcpy(&d.telefoneServidor, &pagina[offset], 14);
+					offset += 14;
 
-					d.nomeServidor = (char *) malloc(d.tamNomeServidor);
-					memcpy(d.nomeServidor, &pagina[offset], d.tamNomeServidor);
-					offset += d.tamNomeServidor - 1;
-				}
-				else{
-					/*caso o campo nao existir, o campo tamNomeServidor adquire valor nulo (-1), que sera utilizado
-					pela funcao printDados para verificar se um valor deve ser impresso ou nao*/
-					d.tamNomeServidor = -1;
-					d.nomeServidor = "";
-				}
-				
-				if(pagina[offset + 4] == 'c'){			//verifica se o campo cargoServidor existe verificando a presenca da tag 'c'
+					if(pagina[offset + 4] == 'n'){		//verifica se o campo nomeServidor existe verificando a presenca da tag 'n'
 						/*caso o campo existir, os dados sao copiados normalmente*/
-						memcpy(&d.tamCargoServidor, &pagina[offset], 4);
+						memcpy(&d.tamNomeServidor, &pagina[offset], 4);
 						offset += 4;
-						memcpy(&d.tagCampo5, &pagina[offset], 1);
+						memcpy(&d.tagCampo4, &pagina[offset], 1);
 						offset += 1;
 
-						d.cargoServidor = (char *) malloc(d.tamCargoServidor);
-						memcpy(d.cargoServidor, &pagina[offset], d.tamCargoServidor);
-						offset += d.tamCargoServidor - 1;
-				}
-				else{
-					/*caso o campo nao existir, o campo tamCargoServidor adquire valor nulo (-1), que sera utilizado
-					pela funcao printDados para verificar se um valor deve ser impresso ou nao*/
-					d.tamCargoServidor = -1;
-					d.cargoServidor = "";
-				}
+						d.nomeServidor = (char *) malloc(d.tamNomeServidor);
+						memcpy(d.nomeServidor, &pagina[offset], d.tamNomeServidor);
+						offset += d.tamNomeServidor - 1;
+					}
+					else{
+						/*caso o campo nao existir, o campo tamNomeServidor adquire valor nulo (-1), que sera utilizado
+						pela funcao printDados para verificar se um valor deve ser impresso ou nao*/
+						d.tamNomeServidor = -1;
+						d.nomeServidor = "";
+					}
+					
+					if(pagina[offset + 4] == 'c'){			//verifica se o campo cargoServidor existe verificando a presenca da tag 'c'
+							/*caso o campo existir, os dados sao copiados normalmente*/
+							memcpy(&d.tamCargoServidor, &pagina[offset], 4);
+							offset += 4;
+							memcpy(&d.tagCampo5, &pagina[offset], 1);
+							offset += 1;
 
-				printDados2(d);		//imprime o registro de dados
+							d.cargoServidor = (char *) malloc(d.tamCargoServidor);
+							memcpy(d.cargoServidor, &pagina[offset], d.tamCargoServidor);
+							offset += d.tamCargoServidor - 1;
+					}
+					else{
+						/*caso o campo nao existir, o campo tamCargoServidor adquire valor nulo (-1), que sera utilizado
+						pela funcao printDados para verificar se um valor deve ser impresso ou nao*/
+						d.tamCargoServidor = -1;
+						d.cargoServidor = "";
+					}
 
-				if(pagina[offset] != '-'){
-					/*o char '-' marca o inicio de um registro de dados (uma vez que nao ha registros removidos).
-					assim, a nao existencia desse char logo apos o fim do registro anterior indica que nao existem
+					printDados2(d);		//imprime o registro de dados
+				}
+				else if(pagina[offset] == '*'){
+					int tamanho;
+					offset += 1;
+					memcpy(&tamanho, &pagina[offset], 4);
+					offset += tamanho + 4;
+					//printf("%x", pagina[offset]);
+				}					
+
+				if(pagina[offset] != '-' && pagina[offset] != '*'){
+					/*os chars '-' e '*' marcam o inicio de um registro de dados.
+					assim, a nao existencia desses chars logo apos o fim do registro anterior indica que nao existem
 					mais registros, marcando assim o fim da pagina de disco.*/
 					paginasAcessadas++;
 					offset = 32000;
 				}
 
-				if(d.tamNomeServidor != -1)
-					free(d.nomeServidor);
+				if(pagina[offset] == '-'){
+					if(d.tamNomeServidor != -1)
+						free(d.nomeServidor);
 
-				if(d.tamCargoServidor != -1)
-					free(d.cargoServidor);
+					if(d.tamCargoServidor != -1)
+						free(d.cargoServidor);
+				}
 			}
 			
 		}
