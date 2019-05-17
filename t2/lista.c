f58f3da11cfbf18478eb81b726365ecb5be1d086
juliafrare <juliafrare7@gmail.com>
2019-05-17 18:32:44 -0300

    adicionado tad lista

---
+++ t2/lista.c
@@ -0,0 +1,125 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lista.h"
+
+void inicializaLista(Lista *l){
+	Node *ptr;
+
+	ptr = (Node *) malloc(sizeof(Node));
+
+	ptr->offset = -1;
+	ptr->size = -1;
+	ptr->prox = NULL;
+
+	l->ini = ptr;
+	l->fim = ptr;
+	l->tamanho = 1;
+}
+
+void insereLista(Lista *l, long int off){
+	Node *ptr;
+	ptr = (Node *) malloc(sizeof(Node));
+
+	if(ptr == NULL)
+	return;
+
+	ptr->offset = off;
+	ptr->prox = NULL;
+	
+	l->fim->prox = ptr;
+	l->fim = ptr;
+	l->tamanho++;
+}
+
+void insereListaTamanho(Lista *l, long int off, int size){
+	Node *ptr, *aux;
+	ptr = (Node *) malloc(sizeof(Node));
+
+	if(ptr == NULL)
+	return;
+
+	ptr->offset = off;
+	ptr->size = size;
+	ptr->prox = NULL;
+	
+	
+	aux = l->ini;
+	while(aux != NULL){
+		if(aux->prox != NULL){
+			if(aux->prox->size >= ptr->size){
+				ptr->prox = aux->prox;
+				aux->prox = ptr;
+				break;
+			}
+		}
+		else{
+			l->fim->prox = ptr;
+			l->fim = ptr;
+			break;
+		}
+		
+		aux = aux->prox;
+	}
+
+	l->tamanho++;
+}
+
+void getLista(FILE *fp, Lista *l){
+	int size;
+	long int off;
+
+	fseek(fp, 1, SEEK_SET);
+
+	fread(&off, 8, 1, fp);
+
+	while(off != -1){
+		insereLista(l, off);
+		fseek(fp, off + 1, SEEK_SET);
+		fread(&size, 4, 1, fp);
+		fread(&off, 8, 1, fp);
+		l->fim->size = size;
+		//printf("%ld %d\n", off, size);
+	}
+
+	//printf("%ld %ld\n", l->ini->offset, l->fim->offset);
+	//printf("%d %d\n", l->ini->size, l->fim->size);
+
+	fseek(fp, 0, SEEK_SET);
+}
+
+void printLista(Lista l){
+	Node *ptr;
+
+	ptr = l.ini;
+
+	while(ptr != NULL){
+		printf("%ld %d\n", ptr->offset, ptr->size);
+		ptr = ptr->prox;
+	}
+}
+
+void getEncadLista(FILE *fp, Lista l){
+	Node *ptr;
+
+	ptr = l.ini;
+	
+	while(ptr != NULL){
+		//printf("%d\n", i);
+		fseek(fp, 0, SEEK_SET);
+
+		if(ptr->prox == NULL){
+			fseek(fp, ptr->offset + 5, SEEK_SET);
+			fwrite(&l.ini->offset, 8, 1, fp);
+		}
+		else if(ptr == l.ini){
+			fseek(fp, 1, SEEK_SET);
+			fwrite(&ptr->prox->offset, 8, 1, fp);
+		}
+		else{
+			fseek(fp, ptr->offset + 5, SEEK_SET);
+			fwrite(&ptr->prox->offset, 8, 1, fp);
+		}
+
+		ptr = ptr->prox;
+	}
+}
---
+++ t2/lista.h
@@ -0,0 +1,16 @@
+typedef struct node{
+	long int offset;
+	int size;
+	struct node *prox;
+}Node;
+
+typedef struct lista{
+	int tamanho;
+	Node *ini, *fim;
+}Lista;
+
+void inicializaLista(Lista *l);
+void insereListaTamanho(Lista *l, long int off, int size);
+void getLista(FILE *fp, Lista *l);
+void printLista(Lista l);
+void getEncadLista(FILE *fp, Lista l);
